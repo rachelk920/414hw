32b68261373d3de516c11029821a73563433a66d
Rachel <rmshinski@gmail.com>
2021-04-14 16:44:40 -0400

    hw

---
+++ hw.js
@@ -0,0 +1,28 @@
+var a = 29;
+var b = 28;
+var c = 27;
+var d = 26;
+var e = 25;
+var f = 24;
+var g = 23;
+var h = 22;
+var i = 21;
+var j = 20;
+
+console.log(b+i);
+console.log(f+j);
+
+console.log(c-h);
+console.log(g-a);
+
+console.log(a*a);
+console.log(d*f);
+
+console.log(j/a);
+console.log(b/b);
+
+console.log(++a);
+console.log(++j);
+
+console.log(--d);
+console.log(--g);
